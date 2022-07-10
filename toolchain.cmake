a90278e7abd7bdefaa995aa2353e558cf5e785d1
Alexej Harm <alexej.h@xiphos.de>
2022-07-10 16:37:01 +0200

    fixed vs include guard

--- toolchain.cmake
+++ toolchain.cmake
@@ -2,7 +2,6 @@
 if(ACE_TOOLCHAIN_LOADED)
   return()
 endif()
-set(ACE_TOOLCHAIN_LOADED ON CACHE STRING "")
 get_filename_component(ACE "${CMAKE_CURRENT_LIST_DIR}" ABSOLUTE CACHE)
 
 # Target
@@ -71,3 +70,6 @@ if(ACE_ENABLE_CCACHE)
   set(CMAKE_CXX_COMPILER_LAUNCHER ${CCACHE} CACHE PATH "")
   unset(CCACHE)
 endif()
+
+# Visual Studio Include Guard
+set(ACE_TOOLCHAIN_LOADED ON CACHE STRING "")
