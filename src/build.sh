7f0b809b644a3af383b2f384793d57d90acfe4d2
Alexej Harm <alexej.h@xiphos.de>
2022-07-13 14:36:51 +0200

    made build scripts resumable

--- src/build.sh
+++ src/build.sh
@@ -2,16 +2,26 @@
 set -e
 
 # Create tools archive.
-make tools
+if [ ! -f tools-linux.tar.gs ]; then
+  make tools
+fi
 
 # Create clang-format archive.
-make clang-format
+if [ ! -f tools-linux-clang-format.tar.gs ]; then
+  make clang-format
+fi
 
 # Create sysroot archive.
-make sys
+if [ ! -f sys-x86_64-pc-linux-gnu.tar.gz ]; then
+  make sys
+fi
 
 # Install ports.
-make ports
+if [ ! -f sys-x86_64-pc-linux-gnu-ports.tar.gz ]; then
+  make ports
+fi
 
 # Create WebAssembly sysroot archive.
-make TARGET=wasm32-wasi sys
+if [ ! -f sys-wasm32-wasi.tar.gz ]; then
+  make TARGET=wasm32-wasi sys
+fi
