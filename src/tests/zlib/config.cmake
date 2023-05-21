5cde124db9b8a7bbee1d4777370be39b4fad076f
Alexej Harm <alexej.h@xiphos.de>
2023-05-21 23:13:07 +0200

    added port: zlib

---
+++ src/tests/zlib/config.cmake
@@ -0,0 +1,2 @@
+find_package(ZLIB REQUIRED)
+target_link_libraries(main PRIVATE ZLIB::ZLIB)
---
+++ src/tests/zlib/main.cpp
@@ -0,0 +1,9 @@
+#include <zlib.h>
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char* argv[])
+{
+  std::cout << "zlib: " << zlibVersion() << std::endl;
+  return EXIT_SUCCESS;
+}
