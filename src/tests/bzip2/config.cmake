4436cde6fa62b0c2c3de75a42f745d885bbf73f1
Alexej Harm <alexej.h@xiphos.de>
2023-05-22 16:57:52 +0200

    bzip2: documented cmake tools search

--- src/tests/bzip2/config.cmake
+++ src/tests/bzip2/config.cmake
@@ -1,2 +1,8 @@
 find_package(BZip2 REQUIRED)
 target_link_libraries(main PRIVATE BZip2::BZip2)
+
+find_program(BZIP2_EXECUTABLE bzip2 REQUIRED)
+message(STATUS "Found bzip2: ${BZIP2_EXECUTABLE}")
+
+find_program(BZIP2RECOVER_EXECUTABLE bzip2recover REQUIRED)
+message(STATUS "Found bzip2recover: ${BZIP2RECOVER_EXECUTABLE}")
