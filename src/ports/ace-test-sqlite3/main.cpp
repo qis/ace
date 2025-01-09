946993e5753e2a1735fde3c58d659404a295dbbe
Alexej Harm <alexej.h@xiphos.de>
2025-01-09 09:31:51 +0300

    moved tests to separate repo

--- src/ports/ace-test-sqlite3/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <sqlite3.h>
-#include <print>
-
-int main(int argc, char* argv[])
-{
-  std::println("SQLite3 Version: {}", sqlite3_libversion());
-}
--- src/ports/ace-test-vulkan/main.cpp
+++
@@ -1,8 +0,0 @@
-// https://github.com/KhronosGroup/KTX-Software/blob/main/examples/vkload.cpp
-#include <volk.h>
-#include <ktxvulkan.h>
-
-int main(int argc, char* argv[])
-{
-  // Possible vcpkg vulkan related port regressions will be tested here in the future.
-}
