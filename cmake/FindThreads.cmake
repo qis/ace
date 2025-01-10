cb31399b3c9cd80c48f21f7ba1f9fe01db7dfefe
Alexej Harm <alexej.h@xiphos.de>
2025-01-10 06:10:41 +0300

    fixed find threads script

--- cmake/FindThreads.cmake
+++ cmake/FindThreads.cmake
@@ -1,16 +1,17 @@
 set(Threads_FOUND TRUE)
-set(CMAKE_THREAD_LIBS_INIT "-pthread")
 
 if(CMAKE_SYSTEM_NAME STREQUAL "Windows")
   set(CMAKE_USE_WIN32_THREADS_INIT 1)
+  set(CMAKE_THREAD_LIBS_INIT "")
 else()
   set(CMAKE_USE_PTHREADS_INIT 1)
+  set(CMAKE_THREAD_LIBS_INIT "-pthread")
 endif()
 
 if(NOT TARGET Threads::Threads)
   add_library(Threads::Threads INTERFACE IMPORTED)
-  set_property(TARGET Threads::Threads PROPERTY INTERFACE_COMPILE_OPTIONS
-    "$<$<COMPILE_LANG_AND_ID:CUDA,NVIDIA>:SHELL:-Xcompiler -pthread>"
-    "$<$<AND:$<NOT:$<COMPILE_LANG_AND_ID:CUDA,NVIDIA>>,$<NOT:$<COMPILE_LANGUAGE:Swift>>>:-pthread>")
-  set_property(TARGET Threads::Threads PROPERTY INTERFACE_LINK_LIBRARIES "${CMAKE_THREAD_LIBS_INIT}")
+  if(CMAKE_USE_PTHREADS_INIT)
+    set_property(TARGET Threads::Threads PROPERTY INTERFACE_COMPILE_OPTIONS "-pthread")
+    set_property(TARGET Threads::Threads PROPERTY INTERFACE_LINK_LIBRARIES "-pthread")
+  endif()
 endif()
