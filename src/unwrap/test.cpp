f38b5e42a3cea7ee9f5a697d550a0a34950c3313
Alexej Harm <alexej.h@xiphos.de>
2025-06-08 10:27:59 +0300

    experimenting with unwrap

--- src/unwrap/test.cpp
+++ src/unwrap/test.cpp
@@ -1,157 +1,198 @@
-#include <stdio.h>
-#include <utility>
-
-#define CONSTEVAL 0
-
-#if CONSTEVAL
-#define puts(...)
-#endif
-
-enum class error {
-  success = 0,  // returned value
-  unknown = 1,  // returned error instead of value
-  failure = 2,  // return error
-  success_never_reached = 1000,
-  failure_never_reached = 1002,
-  move_not_initialized  = 2000,
-};
-
-template <class T>
-struct result {
-  using value_type = T;
-  using error_type = error;
-
-  consteval result() noexcept = default;
-  constexpr result(error_type error) noexcept : error_(error) {}
-  constexpr result(value_type value) noexcept : error_(error::success), value_(value) { puts("value constructed"); }
-
-  constexpr explicit operator bool() const noexcept { return error_ == error::success; }
-  constexpr value_type& value() & noexcept { puts("value_type& value() & [T]"); return value_; }
-  constexpr value_type&& value() && noexcept { puts("value_type&& value() && [T]"); return std::move(value_); }
-  constexpr const value_type& value() const & noexcept { puts("const value_type& value() const & [T]"); return value_; }
-  constexpr error_type error() const noexcept { puts("error_type error() [T]"); return error_; }
-
-private:
-  error_type error_{ error::unknown };
-  value_type value_{};
-};
-
-template <>
-struct result<void> {
-  using value_type = void;
-  using error_type = error;
-
-  consteval result() noexcept = default;
-  constexpr result(error_type error) noexcept : error_(error) {}
-
-  constexpr explicit operator bool() const noexcept { return error_ == error::success; }
-  constexpr error_type error() const noexcept { puts("error_type error() const [void]"); return error_; }
-  constexpr value_type value() const noexcept { puts("value_type value() const [void]"); }
-
-private:
-  error_type error_{ error::success };
-};
+#include "test.hpp"
 
 static int g_argc = 0;
 
-#if CONSTEVAL
+enum class errc {
+  unknown = 0,
+  failure = 1,
+  success_never_reached = 100,
+  failure_never_reached = 101,
+  move_not_initialized = 1000,
+};
 
-consteval result<void> void_success() noexcept {
-  return {};
-}
+constexpr auto error_type = ice::make_error_type(2);
+consteval ice::error_type get_error_type(errc) noexcept { return error_type; }
 
-consteval result<void> void_failure() noexcept {
-  return error::failure;
-}
-
-consteval result<long> long_success() noexcept {
-  return 100;
-}
-
-consteval result<long> long_failure() noexcept {
-  return error::failure;
-}
-
-#else
-
-constexpr result<void> void_success() noexcept {
-  if (g_argc < 0) return error::success_never_reached;
-  return {};
-}
-
-constexpr result<void> void_failure() noexcept {
-  if (g_argc < 0) return error::failure_never_reached;
-  return error::failure;
-}
-
-constexpr result<long> long_success() noexcept {
-  if (g_argc < 0) return error::success_never_reached;
-  return 100;
-}
-
-constexpr result<long> long_failure() noexcept {
-  if (g_argc < 0) return error::failure_never_reached;
-  return error::failure;
-}
-
-#endif
-
-constexpr result<void> test_void() noexcept {
-  __unwrap void_success();
-  __unwrap void_failure();
-  return {};
-}
-
-constexpr result<long> test_long() noexcept {
-  if (__unwrap long_success()) puts("unwrap success works as expected");
-  if (__unwrap long_failure()) puts("unwrap failure should not have worked");
-  return {};
-  //return __unwrap long_success() + __unwrap long_success();
-  //return __unwrap long_success() + __unwrap long_failure();
-  //const auto i0 = __unwrap long_success();
-  //const auto i1 = __unwrap long_failure();
-  //return i0 + i1;
-}
-
+template <bool Consteval>
 class move {
 public:
   consteval move() noexcept = default;
   consteval move(int) noexcept : initialized_(true) {}
 
-  constexpr move(move&& other) noexcept : initialized_(other.initialized_) { puts("move ctor"); }
-  constexpr move(const move& other) noexcept : initialized_(other.initialized_) { puts("copy ctor"); }
-  constexpr move& operator=(move&& other) noexcept { initialized_ = other.initialized_; puts("move ctor"); return *this; }
-  constexpr move& operator=(const move& other) noexcept { initialized_ = other.initialized_; puts("copy ctor"); return *this; }
+  constexpr move(move&& other) noexcept : initialized_(other.initialized_) {
+    if constexpr (!Consteval) puts("move ctor");
+  }
 
-  constexpr bool initialized() const noexcept { puts("testing move"); return initialized_; }
+  constexpr move(const move& other) noexcept : initialized_(other.initialized_) {
+    if constexpr (!Consteval) puts("copy ctor");
+  }
+
+  constexpr move& operator=(move&& other) noexcept {
+    initialized_ = other.initialized_;
+    if constexpr (!Consteval) puts("move op");
+    return *this;
+  }
+
+  constexpr move& operator=(const move& other) noexcept {
+    initialized_ = other.initialized_;
+    if constexpr (!Consteval) puts("copy op");
+    return *this;
+  }
+
+  constexpr bool initialized() const noexcept {
+    return initialized_;
+  }
 
 private:
   bool initialized_ = false;
 };
 
-constexpr result<move> move_success() noexcept {
-  return move{ 1 };
+class consteval_move {
+public:
+  consteval consteval_move() noexcept = default;
+  constexpr consteval_move(int) noexcept : initialized_(true) {}
+
+  constexpr bool initialized() const noexcept { return initialized_; }
+
+private:
+  bool initialized_ = false;
+};
+
+
+
+consteval ice::result<void> void_success() noexcept {
+  return {};
 }
 
-constexpr result<void> test_move() noexcept {
-  const auto v = __unwrap move_success();
-  if (!v.initialized()) {
-    return error::move_not_initialized;
-  }
+static_assert(void_success());
+
+consteval ice::result<void> void_failure() noexcept {
+  return ice::error{ errc::failure };
+}
+
+static_assert(!void_failure());
+static_assert(void_failure().error() == errc::failure);
+
+consteval ice::result<long> long_success() noexcept {
+  return 33;
+}
+
+static_assert(long_success());
+//static_assert(*long_success() == 33);
+
+constexpr ice::result<long> long_failure() noexcept {
+  return ice::error{ errc::failure };
+}
+
+static_assert(!long_failure());
+static_assert(long_failure().error() == errc::failure);
+
+consteval ice::result<consteval_move> consteval_move_success() noexcept {
+  return 33;
+}
+
+static_assert(consteval_move_success());
+//static_assert((*consteval_move_success()).initialized());
+
+constexpr ice::result<consteval_move> consteval_move_failure() noexcept {
+  return ice::error{ errc::failure };
+}
+
+static_assert(!consteval_move_failure());
+static_assert(consteval_move_failure().error() == errc::failure);
+
+consteval ice::result<move<false>> move_success() noexcept {
+  return 33;
+}
+
+static_assert(move_success());
+//static_assert((*move_success()).initialized());
+
+consteval ice::result<move<false>> move_failure() noexcept {
+  return ice::error{ errc::failure };
+}
+
+static_assert(!move_failure());
+static_assert(move_failure().error() == errc::failure);
+
+#if 1
+consteval ice::result<int> func() noexcept {
+  //return ice::error{ errc::failure };
+  return 137;
+}
+#else
+ice::result<int> func() noexcept {
+  return g_argc < 0 ? 137 : 138;
+}
+#endif
+
+#define TEST 0
+constexpr ice::result<void> test() noexcept {
+#if TEST == 0
+  #if 1
+  ice::result<int> v = func();
+  const ice::result<int> c = func();
+  const auto a0 = __unwrap__ func();
+  const auto a1 = __unwrap__ v;
+  const auto a2 = __unwrap__ c;
+  const auto a3 = __unwrap__ std::move(v);
+  const auto a4 = __unwrap__ std::move(c);
+  #else
+  const auto uuuuuuu = __unwrap__ func();
+  const auto mmmmmmm = ({ auto&& __rv = func(); if (!__rv) return __rv.error(); *__rv; });
+  #endif
+#elif TEST == 1
+  constexpr auto r = long_success();
+  // XXX: Append the letter r to the next line to crash clangd.
+  const auto v = __unwrap__ std::move(
+#elif TEST == 2
+  const auto v0 = __unwrap__ void_success();  // error: variable has incomplete type
+  const auto v1 = __unwrap__ void_failure();  // error: variable has incomplete type
+#elif TEST == 3
+  __unwrap__ void_success();
+  __unwrap__ void_failure();
+#elif TEST == 4
+  __unwrap__ long_success();
+  __unwrap__ long_failure();
+#elif TEST == 5
+  constexpr auto r0 = long_success();
+  const auto r1 = long_failure();
+  const auto v0 = __unwrap__ r0;
+  const auto v1 = __unwrap__ r1;
+#elif TEST == 6
+  auto r0 = long_success();
+  const auto v0 = __unwrap__ r0;
+  //auto r1 = long_failure();
+  //const auto v1 = __unwrap__ r1;
+#elif TEST == 7
+  auto r0 = long_success();
+  auto r1 = long_failure();
+  const auto v0 = __unwrap__ std::move(r0);
+  const auto v1 = __unwrap__ std::move(r1);
+#elif TEST == 8
+  __unwrap__ move_success();
+  __unwrap__ move_failure();
+#elif TEST == 9
+  const auto m0 = __unwrap__ move_success();
+  const auto m1 = __unwrap__ move_failure();
+#elif TEST == 10
+  const auto i0 = __unwrap__ long_success(); printf("i0: %ld\n", i0);
+  const auto i1 = __unwrap__ long_success() + __unwrap__ long_success(); printf("i0: %ld\n", i1);
+  const auto v0 = __unwrap__ move_success(); printf("v0: %d\n", v0.initialized() ? 1 : 0);
+  const auto v1 = __unwrap__ move_failure(); printf("v1: %d\n", v1.initialized() ? 1 : 0);
+#endif
+  puts("unreachable");
   return {};
 }
 
 int main(int argc, char* argv[]) {
   g_argc = argc;
-  if (const auto rv = test_void(); !rv) {
-    printf("void error: %d\n", static_cast<int>(rv.error()));
+  if (const auto rv = test(); !rv) {
+    printf("void error: %d\n", static_cast<int>(rv.error().code()));
   }
-  if (const auto rv = test_long(); !rv) {
-    printf("long error: %d\n", static_cast<int>(rv.error()));
-  } else {
-    printf("long value: %d\n", static_cast<int>(rv.value()));
-  }
-  if (const auto rv = test_move(); !rv) {
-    printf("move error: %d\n", static_cast<int>(rv.error()));
-  }
+  //if (const auto rv = test_long(); !rv) {
+  //  printf("long error: %d\n", static_cast<int>(rv.error().code()));
+  //} else {
+  //  printf("long value: %d\n", static_cast<int>(*rv));
+  //}
 }
--- src/unwrap/test.sh
+++ src/unwrap/test.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+#/opt/ace/bin/clang++ -static-libstdc++ -isystem /opt/ace/sys/linux/x86-64-v3/include/c++/v1 -L /opt/ace/sys/linux/x86-64-v3/lib \
+# -Xclang -ast-dump
 /opt/ace/build/llvm-linux-x86-64-v3-debug/bin/clang++ \
-  -std=c++26 -fno-rtti -fno-exceptions -o \
-  /opt/ace/build/llvm-linux-x86-64-v3-debug/test \
+  -std=c++26 -fno-rtti -fno-exceptions -DNDEBUG \
+  -o /opt/ace/build/llvm-linux-x86-64-v3-debug/test \
   /opt/ace/src/unwrap/test.cpp || exit 1
 /opt/ace/build/llvm-linux-x86-64-v3-debug/test
 echo $?
